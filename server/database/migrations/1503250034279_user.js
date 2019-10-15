6b6db8f0bd2453e231740f35f29dec69a1747ba1
Gaël Weiler <aelth@MacBook-Pro-de-Gael.local>
2019-10-15 15:29:16 +0200

    Change hash script for argon2

--- server/database/migrations/1503250034279_user.js
+++ server/database/migrations/1503250034279_user.js
@@ -15,7 +15,7 @@ class UserSchema extends Schema {
         .string('email', 254)
         .notNullable()
         .unique();
-      table.string('password', 60).notNullable();
+      table.string('password', 255).notNullable();
       table.timestamps();
     });
   }
