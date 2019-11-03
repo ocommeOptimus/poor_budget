c20f9a91a2ee452d84a63825cb03ba22e227cab2
GaelWLR <weiler.gael@gmail.com>
2019-11-03 10:45:20 +0100

    Use hook for password hash

--- server/app/Models/User.js
+++ server/app/Models/User.js
@@ -6,15 +6,10 @@ const Hash = use('Hash')
 class User extends Model {
   static boot() {
     super.boot()
-
     /**
      * A hook to hash the user password before saving it to the database.
      */
-    this.addHook('beforeSave', async userInstance => {
-      if (userInstance.dirty.password) {
-        userInstance.password = await Hash.make(userInstance.password)
-      }
-    })
+    this.addHook('beforeSave', 'UserHook.hashPassword')
   }
 
   /**
