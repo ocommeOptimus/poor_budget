66a5404d1415e6340ede086c21a5f39dbd146127
GaelWLR <weiler.gael@gmail.com>
2019-11-06 13:18:26 +0100

    Fixed server, delete second launch

--- server/server.js
+++ server/server.js
@@ -21,11 +21,6 @@ const { Ignitor } = require('@adonisjs/ignitor')
 
 new Ignitor(require('@adonisjs/fold'))
   .appRoot(__dirname)
-  .fireHttpServer()
-  .catch(console.error)
-
-new Ignitor(require('@adonisjs/fold'))
-  .appRoot(__dirname)
   .preLoad('start/acl')
   .fireHttpServer()
   .catch(console.error)
